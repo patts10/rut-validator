46dced8a97e94d1fc415fa5d26cd7102eaa7bf69
Jonathan Christian Mendoza Ramos <patts2810@gmail.com>
2025-04-10 17:47:02 -0500

    feat: udpate version

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Chilean RUT Validator
- * A TypeScript package to validate Chilean RUT (Rol Único Tributario) numbers.
- */
-export { isValidRut, validateRut, formatRut, cleanRut, calculateVerificationDigit, RutValidationResult } from './src/validate-chilean-rut';
